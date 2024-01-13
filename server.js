c7625bf99fa4aff21e90f51cd37d71f97e8422f5
NareshPatilOfficial <nareshpatil.nrp@gmail.com>
2024-01-13 23:10:50 +0530

    Made PORT value as env variable

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const app = require('./app');
 
 //find and test routes of an express application. on -> http://localhost:3000/api-docs
 const sitemap = require('express-sitemap-html');
 sitemap.swagger('My App APIs', app)
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${process.env.PORT}`);
 });
